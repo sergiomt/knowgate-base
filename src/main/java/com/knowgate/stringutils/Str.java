ffd19240b51c89a72805b8cc44e63c6478c00713
Sergio Montoro <sergiom@knowgate.com>
2017-04-23 01:04:24 +0100

    Add Levenstein distance

--- src/main/java/com/knowgate/stringutils/Str.java
+++ src/main/java/com/knowgate/stringutils/Str.java
@@ -151,8 +151,6 @@ public class Str {
 	    return -1;
 	  } // indexOfIgnoreCase
 
-	  // ----------------------------------------------------------
-
 	  /**
 	   * Get substring after a given character sequence
 	   * @param sSource Source String
@@ -301,5 +299,71 @@ public class Str {
 
 	      return new String(aPad) + sSource;
 	  } // leftPad
+
+	  // ----------------------------------------------------------
+
+	  /**
+	   * <p>Calculate Levenshtein distance between two strings</p>
+	   * The Levenshtein distance is defined as the minimal number of characters
+	   * you have to replace, insert or delete to transform str1 into str2.
+	   * The complexity of the algorithm is O(m*n),
+	   * where n and m are the length of str1 and str2.
+	   * @return Levenshtein distance between str1 and str2
+	   * @throws IllegalArgumentException if either str1 or str2 is <b>null</b>
+	   * @author Michael Gilleland & Chas Emerick
+	   * @see <a href="http://www.merriampark.com/ldjava.htm">http://www.merriampark.com/ldjava.htm</a>
+	   * @since 4.0
+	   */
+
+	  public static int levenshteinDistance (String s, String t) {
+	    if (s == null || t == null) {
+	      throw new IllegalArgumentException("Strings must not be null");
+	    }
+	  		
+	    int n = s.length(); // length of s
+	    int m = t.length(); // length of t
+	  		
+	    if (n == 0) {
+	      return m;
+	    } else if (m == 0) {
+	      return n;
+	    }
+	  
+	    int p[] = new int[n+1]; //'previous' cost array, horizontally
+	    int d[] = new int[n+1]; // cost array, horizontally
+	    int _d[]; //placeholder to assist in swapping p and d
+	  
+	    // indexes into strings s and t
+	    int i; // iterates through s
+	    int j; // iterates through t
+	  
+	    char t_j; // jth character of t
+	  
+	    int cost; // cost
+	  
+	    for (i = 0; i<=n; i++) {
+	       p[i] = i;
+	    }
+
+	    for (j = 1; j<=m; j++) {
+	       t_j = t.charAt(j-1);
+	       d[0] = j;
+
+	       for (i=1; i<=n; i++) {
+	          cost = s.charAt(i-1)==t_j ? 0 : 1;
+	          // minimum of cell to the left+1, to the top+1, diagonally left and up +cost				
+	          d[i] = Math.min(Math.min(d[i-1]+1, p[i]+1),  p[i-1]+cost);  
+	       }
+	  
+	       // copy current distance counts to 'previous row' distance counts
+	       _d = p;
+	       p = d;
+	       d = _d;
+	    } 
+
+	    // our last action in the above loop was to switch d and p, so p now 
+	    // actually has the most recent cost counts
+	    return p[n];
+	  } // getLevenshteinDistance
 	  
 }
